43e73bfe1e10f833b7b4304b90c9caf477995282
Note <Note@Note-PC>
2014-07-31 14:29:19 -0300

    Commit inicial.

---
+++ src/br/teste/git/Main.java
@@ -0,0 +1,18 @@
+package br.teste.git;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Note
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Testando e aprendendo Git e GitHub");
+    }
+}
