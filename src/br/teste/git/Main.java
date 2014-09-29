6659d4085beb2bb5969c5b74e6a0cee50b4404f4
Esequias Neto <esequiasneto@brisanet.com.br>
2014-09-29 14:13:01 -0300

    Testando commit

--- src/br/teste/git/Main.java
+++ src/br/teste/git/Main.java
@@ -15,5 +15,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ambiente de teste.");
         System.out.println("Implementação ok.");
+	System.out.println("Teste de Commit");
     }
 }
